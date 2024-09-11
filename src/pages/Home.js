461d73d7c986f7153ab4c9cdd53daa4563d41ae6
maruf19 <marufrony48@gmail.com>
2024-09-11 11:01:30 +0600

    first commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,26 @@
 import React from 'react';
-import img from '../components/Assets/mu.jpeg'
+import img from '../components/Assets/22.jpg'
+import Navbar from '../components/Navbar';
 function Home() {
   return (
-    <section className="relative bg-gray-100 py-20">
+    <>
+    <Navbar/>   
+     <section className="relative bg-gray-100 py-20 ">
       {/* Background Image */}
-      <div className="absolute inset-0 bg-cover bg-center opacity-30">
-      <img src={img} className='w-96'></img>
+      <div className="absolute inset-0 bg-cover bg-center opacity-20 ">
+      <img src={img} alt='background img'className='h-full w-full'></img>
       </div>
       {/* Content */}
-      <div className="relative container mx-auto px-6 text-center">
-        <h1 className="text-4xl font-bold text-teal-500 mb-6">
+      <div className="relative container mx-auto px-6 text-center mt-26">
+        <h1 className="text-4xl font-extrabold text-teal-500 mb-6">
           Welcome to MU CSE Society
         </h1>
         <p className="text-gray-700 text-lg mb-6">
-          We are a community of passionate developers, tech enthusiasts, and
-          learners, striving to make a positive impact in the field of computer science.
-          Join us in our journey of learning, collaboration, and innovation.
+        Welcome to the MU CSE Society, the dynamic hub for Computer Science and Engineering students at MU. Whether you're an experienced coder or just starting out, our society offers a range of opportunities to help you grow, learn, and connect with fellow tech enthusiasts. Our mission is to bridge the gap between academic learning and real-world applications through hands-on workshops, coding challenges, hackathons, and seminars on emerging technologies like AI, cybersecurity, and web development.
+
+         also focus on mentorship and networking, offering valuable connections with industry professionals, alumni, and recruiters from top tech companies. Through career events, tech talks, and collaborative projects, our members build portfolios and develop the skills needed to excel in the tech industry. 
+
+          Joining MU CSE Society means becoming part of a community dedicated to innovation, problem-solving, and shaping the future of technology. Whether you're here to sharpen your skills, work on exciting projects, or expand your professional network, the MU CSE Society is your gateway to success.
         </p>
         <div className="flex justify-center">
           <a
@@ -27,6 +32,7 @@ function Home() {
         </div>
       </div>
     </section>
+    </>
   );
 }
 
