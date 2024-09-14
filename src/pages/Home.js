017ce48dc7548dddb07affacbac6c5d3d7c1fe7c
maruf19 <marufrony48@gmail.com>
2024-09-14 16:20:27 +0600

    1

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,9 +16,9 @@ function Home() {
       {/* Content */}
       <div className="relative container mx-auto px-6 text-center mt-26">
         <h1 className="text-4xl font-extrabold text-teal-500 mb-6">
-          Welcome to MU CSE Society
+          Welcome to MU CSE Society. This is a test
         </h1>
-        <p className="text-gray-700 text-lg mb-6">
+        <p className="text-red-700 text-lg mb-6">
         Welcome to the MU CSE Society, the dynamic hub for Computer Science and Engineering students at MU. Whether you're an experienced coder or just starting out, our society offers a range of opportunities to help you grow, learn, and connect with fellow tech enthusiasts. Our mission is to bridge the gap between academic learning and real-world applications through hands-on workshops, coding challenges, hackathons, and seminars on emerging technologies like AI, cybersecurity, and web development.
 
          also focus on mentorship and networking, offering valuable connections with industry professionals, alumni, and recruiters from top tech companies. Through career events, tech talks, and collaborative projects, our members build portfolios and develop the skills needed to excel in the tech industry. 
